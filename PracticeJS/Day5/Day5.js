76756a1e44ec64d978ef7863845622d9f609e236
LoveKach <cryptonitik@gmail.com>
2023-03-17 09:37:39 +0300

    Day5

--- PracticeJS/Day5/Day5.js
+++ PracticeJS/Day5/Day5.js
@@ -5,3 +5,9 @@ console.log(userHeight);
 userHeight = 181;
 console.log(userHeight);
 console.log(userName + ' is ' + userAge + ' years old');
+
+let nameCar = 'Honda';
+let yearBorn = 2020;
+let engineV = 5;
+
+console.log('I have a ' + nameCar + ' year reelize ' + yearBorn + ' with engine power ' + engineV + ' litres');
