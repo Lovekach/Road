c9d8b505fc5c11844e699018b1baa87320cb08d0
LoveKach <cryptonitik@gmail.com>
2023-03-17 09:24:16 +0300

    Day5

--- PracticeJS/Day5/Day5.js
+++ PracticeJS/Day5/Day5.js
@@ -2,4 +2,6 @@ let userName = 'Valera';
 let userAge = 31;
 let userHeight;
 console.log(userHeight);
+userHeight = 181;
+console.log(userHeight);
 console.log(userName + ' is ' + userAge + ' years old');
