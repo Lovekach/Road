a65170a136da31405d7ab358f0b1eee79e1332b4
LoveKach <cryptonitik@gmail.com>
2023-03-17 10:05:42 +0300

    Day5

--- PracticeJS/Day5/Day5.js
+++ PracticeJS/Day5/Day5.js
@@ -11,3 +11,36 @@ let yearBorn = 2020;
 let engineV = 5;
 
 console.log('I have a ' + nameCar + ' year reelize ' + yearBorn + ' with engine power ' + engineV + ' litres');
+let milesGo;
+console.log(milesGo);
+
+console.log(1 + 3);
+console.log(4 - 2);
+console.log(10 * 3);
+console.log(8 / 4);
+console.log(8 ** 4);
+console.log(8  % 3);
+
+console.log(10 * 0);
+console.log(10 / 0);
+console.log(typeof Infinity);
+
+// Константы в верхнем регистре
+const COLOR_RED = '#F00';
+const COLOR_GREEN = '#0F0';
+const COLOR_BLUE = '#00F';
+// Когда нам нужно выбрать цвет
+let color = COLOR_GREEN;
+console.log(color);
+
+
+const CAPITAL_USA = 'Washington';
+const CAPITAL_FRANCE = 'Paris';
+const CAPITAL_SPAIN = 'Madrid';
+
+let usa = CAPITAL_USA;
+let france = CAPITAL_FRANCE;
+let spain = CAPITAL_SPAIN;
+
+
+console.log('I want to be visit a ' + usa + ' , ' + france + ' , ' + spain + ' it"s my dream');
