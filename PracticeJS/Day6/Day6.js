79ea20d30c977ff523a3c342f2cc8dac03a243f0
LoveKach <cryptonitik@gmail.com>
2023-03-18 10:11:37 +0300

    Day6

---
+++ PracticeJS/Day6/Day6.js
@@ -0,0 +1,10 @@
+// Преобразование типов и базовые операторы
+const a = 3;
+const b = '2';
+console.log(a + b);
+console.log(Number(a) + Number(b));
+
+const c = 3;
+const d = '2a';
+console.log(c + d);
+console.log(Number(c) + Number(d));
