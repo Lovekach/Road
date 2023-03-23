3e42fc0b1095852bcc32a3ecfddb326c71b732d1
LoveKach <cryptonitik@gmail.com>
2023-03-23 09:57:49 +0300

    Day11

---
+++ PracticeJS/Day11/Day11.js
@@ -0,0 +1,27 @@
+function checkAge(age) {
+    if (age < 18) {
+       console.log('you are not allowed');
+    } else {
+        console.log('you are welcome');
+    }
+}
+checkAge(17);
+checkAge(18);
+checkAge(20);
+
+// Задача
+function calc(operation, a, b) {
+  if (operation === 'add') {
+    return a + b;
+  } else if (operation === 'multi') {
+    return a * b;
+  } else if (operation === 'subtract') {
+    return a - b;
+  } else {
+    console.log('Нет данных для операции');
+  }
+}
+
+console.log(calc('add', 1, 2));
+console.log(calc('multi', 1, 2));
+console.log(calc('subtract', 3, 2));
