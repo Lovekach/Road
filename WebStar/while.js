a91eee25db29d442c2dbbf3170a528aa56278472
LoveKach <cryptonitik@gmail.com>
2023-03-27 10:56:35 +0300

    while

--- WebStar/while.js
+++ WebStar/while.js
@@ -1,16 +1,61 @@
 //while
-
 let i = 5;
 
-while(i > 0) {
+while(i < 0) {
     console.log(i);
     i--;
 }
 
 // do while
-let b = -12;
+
+let k = 5;
 
 do {
-    console.log(b);
-    b--;
-} while(b > 0);
+    console.log(k);
+    k--;
+} while(k > 0);
+
+
+// For
+
+for(let i = 0; i <= 10 ; i++) {
+    console.log(i);
+}
+
+let initialValue = 500;
+let counter = 0;
+
+while(true) {
+    if(initialValue < 10) {
+        break;
+    } else {
+        counter++;
+        initialValue /=2;
+    }
+}
+console.log(counter);
+
+// continue
+
+for(let j = 0; j < 10; j++) {
+    if( j % 2 === 0) {
+        continue;
+    }
+    console.log(j);
+}
+
+
+//Задача
+
+let n = 1;
+let m = 3;
+
+let result = 0;
+
+while(n <= m) {
+    result+=n;
+    n++;
+}
+
+console.log(result);
+
