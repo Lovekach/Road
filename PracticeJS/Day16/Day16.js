25445949d4578c7bf68e870386b7bcdbcd90535e
LoveKach <cryptonitik@gmail.com>
2023-03-28 14:27:22 +0300

    Day16

---
+++ PracticeJS/Day16/Day16.js
@@ -0,0 +1,14 @@
+const telegramContact = {
+    Mother: 123,
+    Father: 456,
+    Sister: 789,
+    Other: 4561,
+    
+};
+
+telegramContact["Brother"] = 13579;
+delete telegramContact["Other"];
+
+console.log(telegramContact);
+console.log(telegramContact.Mother);
+console.log(telegramContact["Father"]);
