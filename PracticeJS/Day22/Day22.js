ab83dca6c4065714ef66fa26bc19e9023b15e930
LoveKach <cryptonitik@gmail.com>
2023-04-03 15:46:28 +0300

    2

--- PracticeJS/Day22/Day22.js
+++ PracticeJS/Day22/Day22.js
@@ -57,3 +57,54 @@ console.log( nim.toFixed(1)); // '12.4'
 
 let nam = 12.34;
 console.log( nam.toFixed(5)); // '12.34000'- добавляет недостающие нули
+
+
+// isFinite и isNaN
+
+console.log( isNaN(NaN)); // true
+console.log( isNaN('str')); // true
+
+console.log( NaN === NaN); // false
+
+
+// isFinite(value)
+console.log( isFinite('15')); // true
+console.log( isFinite('str')); // false
+console.log( isFinite(Infinity)); // false
+
+
+
+// Number.isNaN и Number.isFinite
+
+console.log( Number.isFinite(123)); // true
+console.log( Number.isFinite(Infinity)); // false
+console.log( Number.isFinite(2 / 0)); // false
+
+
+// Обратите внимание на разный результат
+console.log( Number.isFinite('123')); // false
+console.log( isFinite('123')); //
+
+
+
+// parseInt и parseFloat
+
+console.log( +'100px'); // NaN
+
+console.log( parseInt('100px')); // 100
+console.log( parseFloat('12.5em')); // 12.5
+
+
+console.log( parseInt('12.3')); // 12
+console.log( parseFloat('12.3.4')); // 12.3
+
+
+// вернут NaN
+
+console.log( parseInt('a123')); // NaN
+
+
+// Способы записи числа
+
+let billion = 1e9; // 1миллиард, буквально: 1 и 9 нулей
+console.log( 7.3e9); // 7.3 миллиардов
--- WebStar/numbers.js
+++ WebStar/numbers.js
@@ -1,1 +1,25 @@
-let billion = 1000000000;
+let billion = 1000000000; // == 1e9
+console.log(billion);
+
+
+let c = 1.4e5;
+console.log(c);
+
+let ms = 1e-5; // 0.00001
+console.log(ms);
+
+
+let n = 500;
+console.log(typeof n);
+console.log(n.toString()); // строка
+
+console.log( 13..toString()); // если нет объявленной переменной то число можно написать через ..toString
+
+// Округление чисел
+let q = 5.121414;
+
+console.log(Math.floor(q)); // 5 округление в меньшую сторону
+console.log(Math.ceil(q)); // 6 округление в  большую строну
+console.log(q.toFixed(2)); // 5.12 округление до n числа знаков после запятой
+
+
