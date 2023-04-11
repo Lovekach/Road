67ef05caa571ffa034ff17e1e009f8aa741e1e77
LoveKach <cryptonitik@gmail.com>
2023-04-11 09:12:52 +0300

    repeat

--- PracticeJS/Day15/Day15.js
+++ PracticeJS/Day15/Day15.js
@@ -2,31 +2,31 @@
 
 let i = 1;
 
-while(i <= 19) {
+while (i <= 19) {
     console.log(i);
     i++;
 }
 
-let j =1;
+let j = 1;
 
 //Перезапись while на for
 
 // 1)
-for(i = 1; i <= 19; i++) {
+for (i = 1; i <= 19; i++) {
     console(i);
 }
 
 // 2)
 let i = 0;
 
-for(; i <= 19; i++) {
+for (; i <= 19; i++) {
     console.log(i);
 }
 
 // 3)
 let i = 0;
 
-for(; i<= 19;) {
+for (; i <= 19;) {
     console.log(i);
     i++;
 }
@@ -36,30 +36,77 @@ for(; i<= 19;) {
 do {
     console.log(j);
     j++;
-} while (j<=19);
+} while (j <= 19);
 
 
 
 //For
 
-for( let k = 1; k < 4; k++) {
+for (let k = 1; k < 4; k++) {
     console.log(k);
 }
 
-for( let l = 0; l < 3; l++) {
+for (let l = 0; l < 3; l++) {
     console.log(l);
 }
 
 let l = 0;
-for(; l < 3; l++) {
+for (; l < 3; l++) {
     console.log(l);
 }
 
-for(; l < 3;) {
+for (; l < 3;) {
     console.log(l);
     l++;
 }
 
-for(;;) {              // Никогда не остановится
+for (; ;) {              // Никогда не остановится
 
-}            
+}
+
+
+
+// Повторение циклов дня 29 
+
+let q = 1;
+while (q < 4) {
+    console.log(q);
+    q++;
+}
+
+
+
+let age = 1;
+while (age < 4) {
+    console.log(age);
+    age++;
+}
+
+
+let w = 1;
+
+while (w <= 19) {
+    console.log(w);
+    w++;
+}
+
+let e = 1;
+do {
+    console.log(e);
+    e++;
+} while (e <= 19);
+
+
+for (let r = 1; r <= 19; r++) {
+    console.log(r);
+}
+
+let t = 1;
+for (; t <= 19; t++) {
+    console.log(t);
+}
+
+for (let y = 1; y <= 19;) {
+    console.log(y);
+    y++;
+}
