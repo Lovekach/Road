86220b82847a0179c0a58948b4cb641356b59557
LoveKach <cryptonitik@gmail.com>
2023-04-11 09:25:14 +0300

    repeaat

--- PracticeJS/Day16/Day16.js
+++ PracticeJS/Day16/Day16.js
@@ -3,7 +3,7 @@ const telegramContact = {
     Father: 456,
     Sister: 789,
     Other: 4561,
-    
+
 };
 
 telegramContact["Brother"] = 13579;
@@ -15,3 +15,37 @@ console.log(telegramContact["Father"]);
 
 
 
+const carList = {
+    'Honda': 2020,
+    'Bmw': 2021,
+    'Audi': 2023,
+    'Mercedes Benz': 2024,
+};
+
+
+carList['Mazda'] = 2022;
+carList.Honda = 2022;
+delete carList['Bmw'];
+
+console.log(carList);
+console.log(carList['Mercedes Benz']);
+console.log(carList.Honda);
+console.log(carList.Bmw);
+console.log(carList.Audi);
+
+
+// Методы объекта
+
+const phoneBook = {
+    list: {
+        'Mother': 123,
+        'Father': 345,
+        'Sister': 567,
+    },
+
+    log() {
+        console.log(this.list);
+    }
+};
+
+phoneBook.log();
