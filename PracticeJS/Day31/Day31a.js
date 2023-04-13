c69780362ec5e1a9929e3ccabee0e84c93b1cf73
LoveKach <cryptonitik@gmail.com>
2023-04-13 15:06:44 +0300

    Day31

--- PracticeJS/Day31/Day31a.js
+++ PracticeJS/Day31/Day31a.js
@@ -2,14 +2,40 @@
 
 
 
-function printNumbers(from, to) {
-    let timerId = setTimeout(function() {
-       console.log(from++);
-        printNumbers(from, to);
-    }, 1000);
-    clearTimeout(timerId);
-    
-}
+// function printNumbers(from, to) {
+//     let current = from;
 
-printNumbers(0, 10);
-dasd
+//     let timerId = setInterval(function() {
+//       console.log(current);
+//       if (current == to) {
+//         clearInterval(timerId);
+//       }
+//       current++;
+//     }, 1000);
+//   }
+
+// printNumbers(0, 5);
+
+// function printNumbers(from, to) {
+//     let current = from;
+
+//     setTimeout(function go() {
+//         console.log(current);
+//         if (current < to) {
+//             setTimeout(go, 1000);
+//         }
+
+
+
+// let timerId = setTimeout(() => console.log('hi'), 2000,);
+// clearTimeout(timerId);
+
+// let timerId = setInterval(() => console.log('hi'), 2000,);
+// setTimeout(() => { clearInterval(timerId); console.log('stop');}, 5000);
+
+
+
+// let timerId = setTimeout(function tick() {
+//     console.log('tick');
+//     timerId = setTimeout(tick, 2000);
+// }, 2000);
