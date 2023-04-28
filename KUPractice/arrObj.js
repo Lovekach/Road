1662e6a9cfc66a719f7f54c8163e77bb2b4c3d9a
LoveKach <cryptonitik@gmail.com>
2023-04-28 18:55:25 +0300

    case

--- KUPractice/arrObj.js
+++ KUPractice/arrObj.js
@@ -55,33 +55,164 @@
 
 
 
-const cryptoList = [{
-    lowRisk: [{
-        avax: '10',
-        atom: '20',
-        xch: '30',
-        dot: '40',
-    }],
+// const cryptoList = [{
+//     lowRisk: [{
+//         avax: '10',
+//         atom: '20',
+//         xch: '30',
+//         dot: '40',
+//     }],
 
-}, {
-    middleRisk: [{
-        mina: '50',
-        algo: '60',
-        xtz: '70',
-        vega: ['80', '90'],
-    }],
+// }, {
+//     middleRisk: [{
+//         mina: '50',
+//         algo: '60',
+//         xtz: '70',
+//         vega: ['80', '90'],
+//     }],
 
-} , {
+// } , {
 
-    highRisk: [{
-        hook: '90',
-        syn: '100',
-        imx: '110',
-        aurora: '120',
-    }],
+//     highRisk: [{
+//         hook: '90',
+//         syn: '100',
+//         imx: '110',
+//         aurora: '120',
+//     }],
+// }
+// ];
+
+// const valueCoin = cryptoList[1].middleRisk[0].vega[1];
+
+// console.log(valueCoin);
+
+
+// -----------------------------------------------------
+
+// Операторы + условия
+
+
+// const news = [
+//     {
+//         title: 'Я сегодня поел',
+//         date: 'Сегодня',
+//         subtitle: 'Я сегодня планирую лечь спать',
+//         content: 'Сегодня был веселый день',
+//         images: ['http:// asdasd', 'http://zxczxc'],
+//         links: ['http:// htgn', 'http://tyutyityixc'],
+
+//         partners: [{
+//             title: 'Мы за все хорошее',
+//             sute: 'http://sazvtrhter2',
+//             keywords: ['Мы хорошие', 'МЫ красивые'],
+//         }, {
+//             title: 'Мы за все хорошее',
+//             sute: 'http://sazvtrhter2',
+//             keywords: ['Мы хорошие', 'МЫ красивые'],
+//         }],
+
+//     },
+// ];
+
+
+// if (!news || news.length === 0) {
+//     console.log('нет новостей');
+// } else {
+
+
+//     const firstNews = news[0];
+//     const secondtNews = news[1];
+
+//     if ('date' in firstNews) {
+//         console.log('Опубликовано' + firstNews.date + '-' + firstNews.title);
+//     };
+//     if (!('date' in secondtNews)) {
+//         console.log('Опубликовано' + secondtNews.title);
+//     }
+
+// }
+
+
+// --------------------------------------
+
+//  SWITCH CASE & ТЕРНАРЫНЙ ОПЕРАТОР 
+const TYPE = {
+    CR: 'critical',
+    NE: 'not_exactly',
+    ST: 'standart,'
 }
+
+
+const news = [
+    {
+        title: 'Я сегодня поел',
+        subtitle: 'Я сегодня планирую лечь спать',
+        content: 'Сегодня был веселый день',
+        type: 'critical',
+        images: ['http:// asdasd', 'http://zxczxc'],
+        links: ['http:// htgn', 'http://tyutyityixc'],
+
+        partners: [{
+            title: 'Мы за все хорошее',
+            sute: 'http://sazvtrhter2',
+            keywords: ['Мы хорошие', 'МЫ красивые'],
+        }, {
+            title: 'Мы за все хорошее',
+            sute: 'http://sazvtrhter2',
+            keywords: ['Мы хорошие', 'МЫ красивые'],
+        }],
+    },
+    {
+        title: 'Я сегодня поелaf',
+        subtitle: 'Курицу',
+        type: 'not_exactly',
+        content: 'Сегодня был весfелый день',
+        images: ['http:// asdasd', 'http://zxczxc'],
+        links: ['http:// htgn', 'http://tyutyityixc'],
+
+        partners: [{
+            title: 'Мы за все хорошее',
+            sute: 'http://sazvtrhter2',
+            keywords: ['Мы хорошие', 'МЫ красивые'],
+        }, {
+            title: 'Мы за все хорошее',
+            sute: 'http://sazvtrhter2',
+            keywords: ['Мы хорошие', 'МЫ красивые'],
+        }],
+
+    },
 ];
 
-const valueCoin = cryptoList[1].middleRisk[0].vega[1];
 
-console.log(valueCoin);
+if (!news || news.length === 0) {
+    console.log('нет новостей');
+} else {
+
+
+    const firstNews = news[0];
+    const secondtNews = news[1];
+}
+
+
+if (firstNews.type === TYPE.CR) {
+    console.error(firstNews.title);
+} else if (firstNews.type === TYPE.NE) {
+    console.warn(firstNews.title);
+}
+
+
+switch (firstNews.type) {
+    case TYPE.CR:
+        console.error(firstNews.title);
+        break;
+    case TYPE.NE:
+        console.warn(firstNews.title);
+        break;
+}
+
+
+let num = 7;
+
+(num = 6) ? 'verno' : 'ne verno';
+
+console.log(num);
