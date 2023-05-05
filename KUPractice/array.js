896a0630530276d8b424b4e05d68f1ad10a5b508
LoveKach <cryptonitik@gmail.com>
2023-05-05 09:17:06 +0300

    ara

---
+++ KUPractice/array.js
@@ -0,0 +1,60 @@
+// Array.isArray()
+//push() - добавить в конец
+//pop() - удалить конец
+
+//unshift() - добавить в начало
+//shift() - удалить начало
+
+//concat()
+//includes()
+//split() и join()
+
+//flat()
+//reverse()
+
+//forEach
+//sort()
+
+//slice(), splice()
+
+
+// const array = [];
+
+// console.log(array);
+// console.log(Array.isArray(array));
+
+// const myArray = [1,2,3];
+// const myArray2 = [1,2,3,5,67,8,9,1];
+
+// const final = myArray.concat(myArray2);
+
+
+// myArray.push(4);
+// myArray.unshift(5);
+
+// myArray.pop();
+// myArray.shift();
+// console.log(final);
+
+
+// const fio = 'Иван Иванович Пупкин';
+
+// const fioArray = fio.split(' ');
+
+// console.log(fioArray[1]);
+
+
+
+// const myArray = [[1, 2] , [3] ,64,  [4] ];
+
+// console.log(myArray.flat());
+
+// const myArray = [1, 2, 3, 4];
+
+// myArray.forEach((element) =>{
+//     console.log(element);
+
+// })
+
+const from = 'lesson, lessons, Для новичков, Создать сайт css,css3,Курсы по css';
+
