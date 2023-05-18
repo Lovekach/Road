99732c897f2bdb0272baaa33ab091d5b97bceeeb
LoveKach <cryptonitik@gmail.com>
2023-05-18 15:42:32 +0300

    asdas

--- KakietoUroki/1.js
+++ KakietoUroki/1.js
@@ -1,28 +1,12 @@
-let userName = 'Valery';
-let userAge = 31;
-let userHeight;
-console.log(userName + ' is ' + userAge + ' years old' );
-
-console.log(userHeight);
-userHeight = 181;
-console.log(userHeight);
-
-console.log(typeof NaN);
-console.log(typeof undefined);
-console.log(typeof null);
-
-console.log(11 % 2);
-console.log(3 ** 5);
-
-let count = 1;
-count++;
-console.log(count);
-
-let counter = 2;
-counter--;
-console.log(counter);
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        let result = length * width * height
+        return result;
+    }
+}
 
 
-let a = 'name';
-let b = true;
-console.log(typeof a + typeof b);
+function otherAngle(a, b) {
+    let third = 180 - (a + b);
+    return third;
+  }
