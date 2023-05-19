c2fafff47ae3c0d155442cc38540732adaddcaa7
LoveKach <cryptonitik@gmail.com>
2023-05-19 15:38:44 +0300

    log

---
+++ KakietoUroki/6.js
@@ -0,0 +1,23 @@
+let name = 'Valeriy'
+
+if (name === 'Valeriy') {
+    console.log('Его имя Валерий');
+} else {
+    console.log('Его не зовут Валерий');
+}
+
+
+let userName = 'Vasya'
+let answer = userName === 'Vitaly' ? 'вы правы, его имя Виталий' : 'вы не правы, его имя не Виталий'
+console.log(answer);
+
+
+
+let age = 18;
+
+let result = age < 3 ? 'Ты еще малыш':
+age < 18 ? 'Ты тоже еще малыш':
+age >= 18 ? 'Ты уже совершеннолетний':
+age > 90 ? 'Ты уже слишком старый' :
+'Ты уже не желец, Вась'
+console.log(result);
