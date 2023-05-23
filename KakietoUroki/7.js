8db8bc6202100bb89ff35f424357aa89a19446c0
LoveKach <cryptonitik@gmail.com>
2023-05-23 15:28:42 +0300

    tern

---
+++ KakietoUroki/7.js
@@ -0,0 +1,27 @@
+const name = 'Tom';
+
+switch (name) {
+    case 'John':
+        console.log('Привет Jonh');
+        break;
+    case 'Jim':
+        console.log('Hi, Jim');
+        break;
+    case 'Jam':
+    case 'Jom':
+        console.log(Hi, guys);
+        break;
+    case 'Alex':
+        console.log('Hi,Alex');
+        break;
+    default:
+        console.log('Nobody say hi');
+}
+
+let limitSpeed = 90;
+
+limitSpeed === 30 ? 'Плетешься как черепаха':
+limitSpeed === 50 ? 'Уже чуть быстрее' :
+limitSpeed === 70 ? 'Еще быстрее':
+limitSpeed === 90 ? 'Максимальная разрешенная скорость':
+'Двигаешься уже с запрещеннйо скоростью';
---
+++ KakietoUroki/9.js
@@ -0,0 +1,23 @@
+const cart = [{
+    name: 'Арбуз',
+    price: 100,
+    count: 1,
+}, {
+    name: 'Хлеб',
+    price: 10,
+    count: 2,
+}, {
+    name: 'МОлоко',
+    price: 50,
+    count: 1,
+}];
+
+let finalPrice = 0;
+
+for (let i = 0; i < length.cart; i++) {
+    const product = cart[i];
+
+    product.price * product.count
+}
+
+
