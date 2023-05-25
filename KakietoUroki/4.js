16a82c833da535b569ade7667ea9799e14223a26
LoveKach <cryptonitik@gmail.com>
2023-05-25 16:48:32 +0300

    umom1

--- KakietoUroki/4.js
+++ KakietoUroki/4.js
@@ -1,8 +1,17 @@
-// const phoneBook = {
-//     'John': 1234,
-//     'Jane Doe': 9834,
-//     'Jim Smith': 111111,
-// };
+const phoneBook = {
+    list: {
+        'John': 1234,
+        'Jane Doe': 9834,
+        'Jim Smith': 111111,
+    }
+    
+};
+
+for ( key in phoneBook.list) {
+    console.log(`${key} - ${phoneBook.list[key]}`);
+}
+
+
 
 // phoneBook['Jim Carry'] = 6574839;
 // phoneBook.John = 190807;
@@ -18,8 +27,8 @@
 //     add(name, number) {
 //         this.list[name] = number;
 //     },
-//     delete(name, number) {
-//         this.list[name] = number;
+//     delete(name) {
+//        delete this.list[name] = number;
 //     }
 // };
 // phoneBook1.add('Sarah Johnson', 444555666);
@@ -39,38 +48,45 @@
 //         this.spisok[name] = number;
 //     },
 
-//     delete(name, number) {
-//         this.spisok[name] = number;
+//     delete(name) {
+//         delete this.spisok[name];
 //     }
 // };
 // myPhoneBook.add('Sestra', 12312564000);
 // console.log(myPhoneBook.spisok);
 
-// myPhoneBook.delete('Brat', 9876);
-// console.log(myPhoneBook);
+// myPhoneBook.delete('Brat');
+// console.log(myPhoneBook.spisok);
+
+// console.log('Mama' in myPhoneBook.spisok);
+
+// for( key in myPhoneBook.spisok) {
+//     // console.log(key);
+// }
+// console.log(`${myPhoneBook.spisok} - ${myPhoneBook.spisok[key]}`);
 
 
-const cart = {
-    money: {
-        sum: 500,
-        currency: 'Руб',
-        allowCurrency: ['Рубли', 'Евро'],
-        discount: {
-            hasDiscount: true,
-            value: 30,
-        },
-    },
-    items: [{
-        title: 'Ручка',
-        count: 2,
-        price: 100,
-    }, {
-        title: 'Книга',
-        count: 1,
-        price: 300,
+// const cart = {
+//     money: {
+//         sum: 500,
+//         currency: 'Руб',
+//         allowCurrency: ['Рубли', 'Евро'],
+//         discount: {
+//             hasDiscount: true,
+//             value: 30,
+//         },
+//     },
+//     items: [{
+//         title: 'Ручка',
+//         count: 2,
+//         price: 100,
+//     }, {
+//         title: 'Книга',
+//         count: 1,
+//         price: 300,
 
-    }],
-};
+//     }],
+// };
 
-console.log(cart['money']['sum']); // === console.log(cart.money.sum)
-console.log(cart.items[1].title);
+// console.log(cart['money']['sum']); // === console.log(cart.money.sum)
+// console.log(cart.items[1].title);
