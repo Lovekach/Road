b3f348689829f6c63a1e8441c3e9f121fb784410
LoveKach <cryptonitik@gmail.com>
2023-06-06 10:50:09 +0300

    umomarr

--- KakietoUroki/3.js
+++ KakietoUroki/3.js
@@ -62,88 +62,173 @@
 // console.log(toDolist9);
 
 
-const users = ['Ivan', 'Petr', 'Anna'];
+// const users = ['Ivan', 'Petr', 'Anna'];
 
-users.forEach(user => {
-    console.log(user);
+// users.forEach(user => {
+//     console.log(user);
+// })
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// numbers.forEach(num => {
+//     console.log(`number is ${num}`);
+// })
+
+
+// const numbers2 = [1, 2, -3, -4, 5, 6];
+// const firstNegativeNumber = numbers2.find(number => number < 0);
+// console.log(firstNegativeNumber);
+
+
+// const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+// const firstLargeWord = animals.find(word => word.length > 7);
+// console.log(firstLargeWord);
+
+// const ani = ['cat', 'lion', 'dog', 'elephant', 'tiger', 'lion'];
+// const secondLion = ani.indexOf('lion', 4)
+// console.log(secondLion);
+
+// const anim = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+// const searchWordInArr = anim.includes('dog');
+// console.log(searchWordInArr);
+
+
+// const count = [1, 11, -2, -3, -10, 0, 4, 3, -9];
+// const onlyNegative = count.filter(num => num < 0);
+// console.log(onlyNegative);
+
+// const diary = [
+//     { name: 'Anna', age: 25 },
+//     { name: 'Any', age: 35 },
+//     { name: 'Bob', age: 15 },
+// ]
+
+// const youngPerson = diary.filter(pers => pers.age < 18);
+// console.log(youngPerson);
+
+
+// const numbe = [1, 11, -2, -3, 4, -10, 5, 8];
+// const plusNumber = numbe.filter(numbr => numbr > 0);
+// console.log(plusNumber);
+
+// const person = ['cat', 'dog', 'monkey', 'horse', 'bird'];
+// const upperName = person.map(pers => pers.toUpperCase());
+// console.log(upperName);
+
+
+// const coun = [1, 11, 3, 5, 66, 24, -12, - 13, -10, 4, 86, 23, 32];
+// const sortedNum = coun.sort((a, b) => a - b);
+// console.log(sortedNum);
+
+
+
+
+// const array = [1, 2, true, 'always', 3, 4];
+// array.forEach((element) => {
+//     console.log(element);
+// });
+
+
+// const from = 'lesson, lessons, Для новичков, Для начинающих, Создать сайт, css, css3, Курсы по css3';
+
+// const splitted = from.split(',');
+// console.log(splitted);
+
+// const final = [];
+
+
+// splitted.forEach(elem => {
+//     const letter = '#' + elem.toLocaleLowerCase();
+//     const changeSymb = elem.split(' ').join('_'); 
+//     console.log(changeSymb);
+
+//     final.push(letter);
+// });
+
+// const str = final.join(' ');
+// console.log(str);
+
+
+
+// const prefix = 'PREF_';
+// const myArray = ['hello', 'hi'];
+
+// myArray.forEach(element => {
+//     console.log(element);
+// });
+
+// const newArr = myArray.map((element1) => {
+//    return  prefix + element1;
+
+// })
+
+
+const data = [{
+    id: 1,
+    title: 'Новость дня',
+    author: 'Alex',
+    likes: 50,
+}, {
+    id: 2,
+    title: 'Важная новость',
+    author: 'Anna',
+    likes: 150,
+}, {
+    id: 3,
+    title: 'Важная новость №2',
+    author: 'John',
+    likes: 140,
+}];
+
+// const moreLikes = data.filter((elem) => {
+//     // console.log(elem);
+//     if (elem.likes > 100) {
+//         return true;
+//     }
+
+//     // return elem.likes > 100 ? true : false;    или return elem.likes > 100;
+
+// })
+
+// console.log(moreLikes);
+
+// const data1 = moreLikes.map((elem) => {
+
+//     return {
+//         id: elem.id,
+//         title: elem.title
+//     }
+// })
+
+// console.log(data1);
+
+// const data2 = moreLikes.map((elem) => {
+    
+//     return elem.author;
+// })
+
+// console.log(data2);
+
+const allLikes = data.map((elem) => {
+    return {
+        author: elem.author,
+        like: elem.likes,
+    }
+        
 })
 
+console.log(allLikes);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-numbers.forEach(num => {
-    console.log(`number is ${num}`);
-})
-
-
-const numbers2 = [1, 2, -3, -4, 5, 6];
-const firstNegativeNumber = numbers2.find(number => number < 0);
-console.log(firstNegativeNumber);
-
-
-const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
-const firstLargeWord = animals.find(word => word.length > 7);
-console.log(firstLargeWord);
-
-const ani = ['cat', 'lion', 'dog', 'elephant', 'tiger', 'lion'];
-const secondLion = ani.indexOf('lion', 4)
-console.log(secondLion);
-
-const anim = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
-const searchWordInArr = anim.includes('dog');
-console.log(searchWordInArr);
-
-
-const count = [1, 11, -2, -3, -10, 0, 4, 3, -9];
-const onlyNegative = count.filter(num => num < 0);
-console.log(onlyNegative);
-
-const diary = [
-    { name: 'Anna', age: 25 },
-    { name: 'Any', age: 35 },
-    { name: 'Bob', age: 15 },
-]
-
-const youngPerson = diary.filter(pers => pers.age < 18);
-console.log(youngPerson);
-
-
-const numbe = [1, 11, -2, -3, 4, -10, 5, 8];
-const plusNumber = numbe.filter(numbr => numbr > 0);
-console.log(plusNumber);
-
-const person = ['cat', 'dog', 'monkey', 'horse', 'bird'];
-const upperName = person.map(pers => pers.toUpperCase());
-console.log(upperName);
-
-
-const coun = [1, 11, 3, 5, 66, 24, -12, - 13, -10, 4, 86, 23, 32];
-const sortedNum = coun.sort((a, b) => a - b);
-console.log(sortedNum);
-
-
-
-
-const array = [1, 2, true, 'always', 3, 4];
-array.forEach((element) => {
-    console.log(element);
+const bestAuthor = allLikes.filter((elem) => {
+    if (elem.like > 140) {
+        return {
+            author:elem.author,
+            like: elem.like
+        }
+    }
 });
+console.log(bestAuthor);
 
 
-const from = 'lesson, lessons, Для новичков, Для начинающих, Создать сайт, css, css3, Курсы по css3';
-
-const splitted = from.split(',');
-console.log(splitted);
-
-const final = [];
-
-
-splitted.forEach(elem => {
-    const letter = '#' + elem.toLocaleLowerCase();
-    const changeSymb = elem.split(' ').join('_'); 
-    console.log(changeSymb);
-
-    final.push(letter);
-});
-
-const str = final.join(' ');
-console.log(str);
+const nameBestAuthor = bestAuthor.map(name => name.author);
+console.log(nameBestAuthor);
