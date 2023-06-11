dfe08947fb0440fc1f6ccd4d57145bbb5277cb5b
LoveKach <cryptonitik@gmail.com>
2023-06-11 10:15:28 +0300

    umom

---
+++ KakietoUroki/23.js
@@ -0,0 +1,77 @@
+// const test = ['Апельсин', 'Банан', 'Персик'];
+
+// const result = test.reduce((acc, elem) => {
+//     acc[`name - ${elem}`] = elem;
+//     return acc;
+// }, {});
+
+// const result = test.reduce((acc, item) => {
+//     acc += item; 
+//     return acc;
+// },'Фрукты: ');
+
+// console.log(result);
+
+
+
+// const test = [1, 2, 3, 4, 5];
+
+// const result = test.reduce((acc,elem) => {
+//     acc += elem; 
+//     return acc;
+// }, 0)
+
+
+// const result = test.reduce((acc, elem) => {
+//     acc.push(elem.toString());
+//     return acc;
+// }, []);
+
+// const result = [];
+
+// test.forEach((elem) => {
+//     result.push(elem.toString());
+// });
+
+// console.log(result);
+
+
+// const test2 = [
+//     ['Apple'],
+//     ['Pineapple'],
+//     ['Orange'],
+// ];
+
+// const result2 = [];
+// test2.forEach((elem) => {
+//     result2.push(elem[0]);
+
+// })
+
+
+// const result2 = test2.reduce((acc, elem) => {
+//     acc.push(elem[0]);
+//     return acc;
+// }, []);
+
+
+
+
+// console.log(test2.flat());
+// console.log(result2);
+
+
+const goodProduct = ['Салат', 'ВОда', 'Шоколад', 'Булка']
+
+const test3 = [{
+    tittle: 'Сникерс',
+    composition: ['Шоколад', 'Сaхар', 'Нуга', 'Что-то еще'],
+}, {
+    tittle: 'Бургер',
+    composition: ['Булка', 'Котлета', 'Салат', 'Другие овощи'],
+}, {
+    tittle: 'Липтон',
+    composition: ['Вода', 'Сахар', 'Сок лимона', 'Щепотка магии'],
+}];
+
+
