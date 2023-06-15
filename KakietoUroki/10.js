1818e7e35dd4e5f6e7044817ecfc051ab69e00a8
LoveKach <cryptonitik@gmail.com>
2023-06-15 10:37:03 +0300

    umom

--- KakietoUroki/10.js
+++ KakietoUroki/10.js
@@ -1,23 +1,76 @@
-const printHello = (text) => {
-    console.log(text);
-};
+// const printHello = (text) => {
+//     console.log(text);
+// };
 
-setTimeout(() => {
-    printHello('НАш текст')
-}, 3000)
+// setTimeout(() => {
+//     printHello('НАш текст')
+// }, 3000)
 
-let timerId = setTimeout(printHello, 2000, 'Привет');
+// let timerId = setTimeout(printHello, 2000, 'Привет');
 
-clearTimeout(timerId);
+// clearTimeout(timerId);
 
 
-let timerId2 = setInterval(printHello, 2000, 'Интервал');
+// let timerId2 = setInterval(printHello, 2000, 'Интервал');
 
-const customSetInterval = (func, interval, params) => {
-    setTimeout(() => {
-        func(params);
-        customSetInterval = (func, interval, params);
-    }, interval);
-};
+// const customSetInterval = (func, interval, params) => {
+//     setTimeout(() => {
+//         func(params);
+//         customSetInterval = (func, interval, params);
+//     }, interval);
+// };
 
-customSetInterval();
+// customSetInterval();
+
+// const timer = setTimeout(function() {
+//     console.log('Прошло 2 секунды');
+
+// }, 2000);
+
+// function logger() {
+//     console.log('Прошло 3 секунды');
+// }
+
+// const nimerId = setTimeout(logger, 3000);
+
+// let secunda = 0;
+// function secundomer(secunda) {
+//     console.log(secunda++);
+// }
+
+// setInterval(secundomer, 1000)
+
+// let timerId = setInterval(() {
+//     console.log('tick');
+// },2000);
+
+// setTimeout(() {
+//     clearInterval(timerId);
+//     console.log('Stop');
+// },5000);
+
+
+// function printNumbers(from, to) {
+//     let current = from;
+
+//     let timerId = setInterval(function() {
+//         console.log(current);
+//         if (current === to) {
+//             clearInterval(timerId);
+//         }
+//         current++
+//     }, 1000);
+// }
+
+// printNumbers(0, 10);
+
+setTimeout(function go() {
+    console.log(current);
+    if (current < to) {
+        setTimeout(go, 1000)
+    }
+    current++;
+
+}, 1000);
+
+printNumbers(0, 10);
