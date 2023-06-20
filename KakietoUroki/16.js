f77d36efa60f7e5e340f2f5a354abb52f8d9cc81
LoveKach <cryptonitik@gmail.com>
2023-06-20 15:27:01 +0300

    umom

--- KakietoUroki/16.js
+++ KakietoUroki/16.js
@@ -29,6 +29,23 @@
 
 // console.log(document.body.parentElement);
 
-let result = document.getElementById('ulika');
-ulika.style.color = 'red'
-console.log(result);
+// let result = document.getElementById('ulika');
+// ulika.style.color = 'red'
+// console.log(result);
+
+
+let cards = [{
+    id:1, balance: 5000, currency: 'USD'
+} , {
+    id:2, balance: 3000, currency: 'EUR'
+}, {
+    id:3, balance: 6000, currency: 'RUB'
+}];
+
+function clickHandler() {
+    cards.forEach(card => {
+        console.log(`Card ID: ${card.id}, Balance: ${card.balance}, Currency: ${card.currency}`);
+    });
+}
+
+document.getElementById('button').addEventListener('click', clickHandler);
