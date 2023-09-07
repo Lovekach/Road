44f31293141153c7d4b904349229bfa05aa1e37f
LoveKach <cryptonitik@gmail.com>
2023-09-07 20:13:19 +0300

    added

---
+++ JSpapka/codewars2.js
@@ -0,0 +1,77 @@
+// const user = {
+//     name: 'Alex',
+//     age: 18,
+//     state: null,
+//     family: {
+//         dad: {
+//             name: 'Pavel',
+//             age: 50,
+//         },
+//         mom: {
+//             name: 'Elena',
+//             age: 61,
+//         },
+//     }
+// }
+
+
+// const str = JSON.stringify(user);
+// console.log(JSON.parse(str));
+
+// for ( let key in user.family) {
+//     const value = user.family[key];
+//     console.log(`${key}: ${value}`)
+// }
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+
+// const entries = Object.entries(user);
+
+// for (let i = 0;i < entries.length; i++) {
+//    const value = entries[i];
+//    console.log(value); 
+//    const key = value[0];
+//    const item = value[1];
+//    console.log(key, item);
+// }
+
+
+
+// const obj1 = {name:'Alex'}
+// const obj2 = {name:'Alex'}
+
+// console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
+
+
+
+const userInformation = {
+    name: 'Иван',
+    surname: 'Иванов',
+    age: 15,
+    family: {
+        dad: {
+            name: 'Александр',
+            surname: 'Иванов',
+            age: undefined,
+        },
+         mom: {
+            name: 'Наталья',
+            surname: 'Иванова',
+        },
+         brother: {
+            name: 'Константин',
+            surname: 'Иванов',
+            age: '10',
+        },
+        sister: {
+            name: 'Константин',
+            surname: 'Иванов',
+            age: '10',
+        },
+   }
+ }
+
+const letter = addEventListener.apply();
